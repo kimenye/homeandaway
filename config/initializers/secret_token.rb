0af58d30f3f3f87c4d3ddf3d9bd4b8e7dc2e6c4d
Trevor Kimenye <kimenye@gmail.com>
2012-11-12 18:51:51 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homeandaway::Application.config.secret_token = '9c62aab95e2d8bd377954cb943e9279d9eb37d1b5f526cf73fa47efcbeae8c203d8c7aab79037b3bcf1278bc4afe9796c6e78d9e63372bf44baac8239e8460cb'
