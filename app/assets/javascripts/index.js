28793a8e7caa8da9511c430b0316960f276a6a58
Trevor Kimenye <kimenye@gmail.com>
2012-11-17 21:45:46 +0300

    Fixed drop down button

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
-    $('.vote-link').click(function(link) {
-        var link = $(this);
-        var id = link.data('id');
+//    $('.vote-link').click(function(link) {
+//        var link = $(this);
+//        var id = link.data('id');
+//
+//        var url = "/nominees/" + id + "/votes";
+//
+//        $.post(url, function(result) {
+//            link.remove();
+//        });
+//    });
 
-        var url = "/nominees/" + id + "/votes";
-
-        $.post(url, function(result) {
-            link.remove();
-        });
+    $('.icon-drop-down').click(function() {
+        console.log("Clicked the drop down", $('.login-or-register'));
+        $('.login-or-register').removeClass('hidden').transition({ y: '265px' });
     });
 });
